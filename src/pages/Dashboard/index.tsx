0bdc36ceaa60a744e148f1b786eec928a7f88ae9
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-15 18:49:37 -0300

    Created and styled - dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logo} alt="" srcSet="" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pequisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/6005658?s=460&u=fb81da823b66c87d38d58bbfddebc571f3fc140d&v=4"
+            alt="Italo Fernandes"
+          />
+          <div>
+            <strong>Rocketseat / unform</strong>
+            <p>Performance-focused API for React forms </p>
+          </div>
+
+          <FiChevronRight size="20" />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
