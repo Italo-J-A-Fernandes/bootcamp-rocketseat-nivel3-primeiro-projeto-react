8e47deed9cdb55bbddc7660744667407c63d210f
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-16 02:55:12 -0300

    navigating between dashboard and repository detail

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
@@ -18,7 +19,16 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplore:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
 
   useEffect(() => {
     localStorage.setItem(
@@ -58,7 +68,7 @@ const Dashboard: React.FC = () => {
   }
   return (
     <>
-      <img src={logo} alt="" srcSet="" />
+      <img src={logo} alt="Logo da aplicação" />
       <Title>Explore repositórios no Github</Title>
       <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         {/* outra forma de transformar a propriedade em um boolean => hasError={Boolean(inputError)} */}
@@ -75,7 +85,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -86,7 +99,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size="20" />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
